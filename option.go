803d7d22d97a73f6ec6c5cfa9b6d879e0033c83f
Aveline Swan <i@vii.im>
2014-05-17 05:59:40 +0800

    prototype

---
+++ option.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"github.com/ym/oss-aliyun-go"
+	"log"
+	"os"
+	"strings"
+)
+
+type Config struct {
+	endpoint         string
+	internal         bool
+	region           string
+	source           string
+	bucket           string
+	prefix           string
+	accessId         string
+	accessKey        string
+	deleteIfNotFound bool
+}
+
+var (
+	config Config
+)
+
+func getOptions() {
+	config = Config{
+		endpoint: oss.DefaultEndpoint,
+		internal: false,
+		region:   "hangzhou",
+	}
+
+	flag.StringVar(&config.endpoint, "endpoint", oss.DefaultEndpoint, "oss api endpoint")
+	flag.BoolVar(&config.internal, "internal", false, "use internal ip address of oss api")
+	flag.StringVar(&config.region, "region", oss.DefaultRegion, "oss api region [hangzhou,qingdao,beijing,hongkong]")
+	flag.StringVar(&config.source, "source", "", "source directory")
+	flag.StringVar(&config.bucket, "bucket", "", "dest bucket")
+	flag.StringVar(&config.prefix, "prefix", "", "dest prefix")
+	flag.StringVar(&config.accessId, "key", "", "access key")
+	flag.StringVar(&config.accessKey, "secret", "", "access secret")
+	flag.BoolVar(&config.deleteIfNotFound, "delete", false, "delete extraneous files from dest bucket")
+
+	flag.Parse()
+
+	if config.endpoint == oss.DefaultEndpoint {
+		region := strings.ToLower(config.region)
+		switch region {
+		case "hangzhou":
+			if config.internal {
+				config.endpoint = oss.HangzhouInternal
+			} else {
+				config.endpoint = oss.Hangzhou
+			}
+		case "qingdao":
+			if config.internal {
+				config.endpoint = oss.QingdaoInternal
+			} else {
+				config.endpoint = oss.Qingdao
+			}
+		case "beijing":
+			if config.internal {
+				config.endpoint = oss.BeijingInternal
+			} else {
+				config.endpoint = oss.Beijing
+			}
+		case "hongkong":
+			if config.internal {
+				config.endpoint = oss.HongkongInternal
+			} else {
+				config.endpoint = oss.Hongkong
+			}
+		default:
+			log.Fatalf("Region %s not defined.", config.region)
+		}
+	}
+
+	if config.accessId == "" || config.accessKey == "" {
+		log.Fatalln("Access key and access secret must be specified")
+	}
+
+	if strings.HasPrefix(config.prefix, "/") {
+		config.prefix = strings.TrimLeft(config.prefix, "/")
+	}
+
+	if strings.HasSuffix(config.source, "/") {
+		config.source = strings.TrimRight(config.source, "/")
+	}
+
+	fi, err := os.Stat(config.source)
+	if err != nil {
+		log.Fatalf("Unable to stat source directory %s: %s.\n", config.source, err.Error())
+	}
+	if fi.Mode().IsDir() == false {
+		log.Fatalf("Source %s not a directory\n", config.source)
+	}
+}
---
+++ sync.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"github.com/ym/oss-aliyun-go"
+	"io/ioutil"
+	"log"
+	"mime"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	a   oss.Auth
+	o   *oss.OSS
+	b   *oss.Bucket
+	err error
+)
+
+func syncFiles() {
+	a = oss.Auth{config.accessId, config.accessKey}
+	o = &oss.OSS{a, config.endpoint, config.endpoint}
+
+	b = o.Bucket("apt-public")
+
+	remoteFiles, _ := getRemoteFiles()
+	localFiles, _ := getLocalFiles()
+
+	for path, file := range localFiles {
+		fn := config.prefix + strings.TrimLeft(strings.TrimLeft(path, config.source), "/")
+		if remote, ok := remoteFiles[fn]; ok {
+			log.Printf("Remote file %s existing, calculating local etag %s.\n", fn, remote.ETag)
+		} else {
+			if file.Mode().IsDir() == true {
+				putDirectory(fn)
+			} else {
+				putFile(path, fn)
+			}
+		}
+	}
+}
+
+func putDirectory(filename string) {
+	data := []byte("")
+	err := b.Put(filename, data, "text/plain", oss.Private)
+	if err != nil {
+		log.Printf("Unable to create directory %s: %s.\n", filename, err.Error())
+	} else {
+		log.Printf("Created directory %s.\n", filename)
+	}
+}
+
+func putFile(path string, filename string) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("Unable to read local file %s: %s.\n", path, err.Error())
+	}
+	filetype := mime.TypeByExtension(filepath.Ext(path))
+	if filetype == "" {
+		filetype = "application/octet-stream"
+	}
+	err = b.Put(filename, data, filetype, oss.Private)
+	if err != nil {
+		log.Printf("Unable to upload file %s: %s.\n", filename, err.Error())
+	} else {
+		log.Printf("Uploaded file: %s ... ", filename)
+	}
+}
+
+func getRemoteFiles() (files map[string]oss.ContentInfo, err error) {
+	var (
+		marker = ""
+		max    = 512
+		get    func() error
+	)
+
+	files = make(map[string]oss.ContentInfo)
+
+	get = func() error {
+		remoteFiles, err := b.List(config.prefix, "/", marker, max)
+		if err != nil {
+			return err
+		}
+		for _, file := range remoteFiles.Contents {
+			files[file.Key] = file
+		}
+		if remoteFiles.IsTruncated {
+			marker = remoteFiles.NextMarker
+			get()
+		}
+		return nil
+	}
+
+	err = get()
+	if err != nil {
+		return files, err
+	}
+
+	return files, nil
+}
+
+func getLocalFiles() (files map[string]os.FileInfo, err error) {
+	files = make(map[string]os.FileInfo)
+	err = filepath.Walk(config.source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("%s while looking up %s", err.Error(), path)
+			return err
+		}
+		files[path] = info
+		return nil
+	})
+	return files, err
+}
